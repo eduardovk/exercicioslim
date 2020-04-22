02c654d1b2dd68991d78f710da8601d158c5b0f3
eduardovk <eduardo_vk@hotmail.com>
2020-04-22 17:25:48 -0300

    Modificada sintaxe em arquivo do Doctrine para corrigir warnings

--- vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php
+++ vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php
@@ -2621,7 +2621,8 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, $data[$field]);
                         $this->originalEntityData[$oid][$field] = $data[$field];
 
-                        continue;
+                        //continue; <-original
+                        continue 2;
                     }
 
                     $associatedId = array();
@@ -2650,7 +2651,8 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, null);
                         $this->originalEntityData[$oid][$field] = null;
 
-                        continue;
+                        //continue; <-original
+                        continue 2;
                     }
 
                     if ( ! isset($hints['fetchMode'][$class->name][$field])) {
